ca62f0e57740e789c22df5a18b242f14a556a4bd
North2299 <mariem.zouari@enis.tn>
2022-06-13 11:06:53 +0200

    adding k8s jmeter configuration files

--- Jmeter/k8s/start-test.sh
+++ Jmeter/k8s/start-test.sh
@@ -4,7 +4,7 @@ test_name="$(basename "$test_file_path")"
 
 #Get Master pod details
 
-master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+master_pod=`kubectl get pod -n jmeter | grep jmeter-master | awk '{print $1}'`
 
 kubectl cp "$test_file_path" -n jmeter "$master_pod:/$test_name"
 
