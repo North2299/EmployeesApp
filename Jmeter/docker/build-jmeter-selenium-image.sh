38d7ab910a9668ab4abb572106f15191afbe94af
North2299 <mariem.zouari@enis.tn>
2022-06-16 10:46:17 +0200

    Adding jmeter with selenium docker file

---
+++ Jmeter/docker/build-jmeter-selenium-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker build -t mariemzouari/chromejmeter -f Dockerfile-Selenium
+#give the path to the folder  where you have your jmx file
+export volume_path="../senario/testplan2.jmx"
+#give the path within your container
+export jmeter_path="/jmeter/sample/"
+docker run --name mariemzouari/chromejmeter --volume "${volume_path}":${jmeter_path} jmeterdocker -n -t ${jmeter_path}/testplan2.jmx -l tmp/result_${NOW}.jtl -j tmp/jmeter_${NOW}.log
