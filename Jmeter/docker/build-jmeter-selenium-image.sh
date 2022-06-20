1b43e988f5f47ccf90e22bc9cb9d7baa6ab4a2ef
North2299 <mariem.zouari@enis.tn>
2022-06-20 12:41:29 +0200

    updating jmeter selenium dockerfile

--- Jmeter/docker/build-jmeter-selenium-image.sh
+++ Jmeter/docker/build-jmeter-selenium-image.sh
@@ -4,5 +4,5 @@ docker build --tag="mariemzouari/chromejmeter" -f Dockerfile-Selenium .
 #give the path to the folder  where you have your jmx file
 export volume_path="/home/userpfe/EmployeesApp/Jmeter/senario/testplan2.jmx"
 #give the path within your container
-export jmeter_path="/opt/apache-jmeter-*/"
+export jmeter_path="/opt/apache-jmeter-5.4.3/"
 docker run --name jmeter-selenium  --volume "${volume_path}":${jmeter_path} mariemzouari/chromejmeter -n -t ${jmeter_path}/testplan2.jmx -l tmp/result_${NOW}.jtl -j tmp/jmeter_${NOW}.log
