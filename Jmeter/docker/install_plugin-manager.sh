fbe45875f63fe25cf6f469366d31505c9df511d3
North2299 <mariem.zouari@enis.tn>
2022-06-26 23:21:25 +0200

    deploy Seleniumgrid on kubernetes

--- Jmeter/docker/install_plugin-manager.sh
+++ Jmeter/docker/install_plugin-manager.sh
@@ -4,13 +4,13 @@ CMD_RUNNER_URL="http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/
 PLUGIN_MANAGER_URL="http://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/1.7/jmeter-plugins-manager-1.7.jar"
 
 # DOWNLOAD RUNNER
-wget -O "lib/cmdrunner-2.2.1.jar" $CMD_RUNNER_URL
+wget -O " /jmeter/apache-jmeter-5.4.3/lib/cmdrunner-2.2.1.jar" $CMD_RUNNER_URL
 
 # DOWNLOAD MANAGER
-wget -O "lib/ext/jmeter-plugins-manager.jar" $PLUGIN_MANAGER_URL
+wget -O " /jmeter/apache-jmeter-5.4.3/lib/ext/jmeter-plugins-manager.jar" $PLUGIN_MANAGER_URL
 
 
 # GENERATING SCRIPTS
-java -cp lib/ext/jmeter-plugins-manager.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+java -cp  /jmeter/apache-jmeter-5.4.3/lib/ext/jmeter-plugins-manager.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 PluginsManagerCMD.sh install jpgc-webdriver=3.3
 
